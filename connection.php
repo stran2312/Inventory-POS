fb45a3def9e3b15931103b29fb35b56682b2926b
Sang Tran <100749058+stran2312@users.noreply.github.com>
2022-07-16 01:04:32 -0500

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php 
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=pos_db','root','');
+    echo 'Connection successful';
+} catch (PDOException $f ) {
+    echo $f->getmessage();
+}
+
+?>
---
+++ dashboard.php
@@ -0,0 +1,53 @@
+<?php 
+include_once 'header.php';
+
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Admin Dashboard
+        
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+ <?php 
+ include_once 'footer.php';
+ ?>
+ 
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+  immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 3 -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+     Both of these plugins are recommended to enhance the
+     user experience. -->
+</body>
+</html>
